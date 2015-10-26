0a87dc57560daa0b0ce94edf19b8f247d266bc5a
Vyacheslav Zaikin <agitto@yandex.ru>
2015-10-26 17:39:59 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConsoleApp3 {
+    public class Program {
+        public void Main(string[] args) {
+        	SqlConnection connection = new SqlConnection();
+        }
+    }
+}
